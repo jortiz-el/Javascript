17a3628bd7eb92af634ad8aa91b17c9e07e6c11c
dav <dvillaluenga@gmail.com>
2015-10-14 12:30:34 -0700

    hola

--- proyecto_fecha/test/js/test.calculatedays.js
+++ proyecto_fecha/test/js/test.calculatedays.js
@@ -5,14 +5,14 @@ describe('Check primalty', function() {
             it('should return FORMAT_ERROR for empty string', function () {
             	calculateDaysSinceEpoch("").should.equal("Por favor, introducir la fecha en el formato solicitado");
             });
+             it('should return FORMAT_ERROR for "MANOLO"', function () {
+                calculateDaysSinceEpoch("MANOLO").should.equal("Por favor, introducir la fecha en el formato solicitado");
+            });
             it('should return FORMAT_ERROR for "32jan2014"', function () {
             	calculateDaysSinceEpoch("32jan2014").should.equal("Por favor, introducir la fecha en el formato solicitado");
             });
             it('should return FORMAT_ERROR for "31/12/2016"', function () {
                 calculateDaysSinceEpoch("31/12/2016").should.equal("Por favor, introducir la fecha en el formato solicitado");
-            });
-            it('should return FORMAT_ERROR for "31dec1969"', function () {
-                calculateDaysSinceEpoch("31dec1969").should.equal("Por favor, introducir la fecha en el formato solicitado");
             });
             it('should return FORMAT_ERROR for "31ene2015"', function () {
                 calculateDaysSinceEpoch("31ene2015").should.equal("Por favor, introducir la fecha en el formato solicitado");
@@ -31,10 +31,13 @@ describe('Check primalty', function() {
             });
         });
         context('Errores de fecha', function() {
+            it('should return DATE_ERROR for "31dec1969"', function () {
+                calculateDaysSinceEpoch("31dec1969").should.equal("La fecha introducida no es válida");
+            });
             it('should return DATE_ERROR for "29feb2011"', function () {
                 calculateDaysSinceEpoch("29feb2011").should.equal("La fecha introducida no es válida");
             });
-            it('should return DATE_ERROR for "29feb2100"', function () {
+            it('should return DATE_ERROR for "29feb2300"', function () {
                 calculateDaysSinceEpoch("29feb2300").should.equal("La fecha introducida no es válida");
             });
             it('should return DATE_ERROR for "31apr1970"', function () {
@@ -46,16 +49,16 @@ describe('Check primalty', function() {
         });
         context('Fechas correctas', function() {
             it('should return "741.441" for "31dec3999"', function () {
-                calculateDaysSinceEpoch("31dec3999").should.equal("741441");
+                calculateDaysSinceEpoch("31dec3999").should.equal(741441);
             });
             it('should return "0" for "01jan1970"', function () {
-                calculateDaysSinceEpoch("01jan1970").should.equal("0");
+                calculateDaysSinceEpoch("01jan1970").should.equal(0);
             });
             it('should return "1" for "02jan1970"', function () {
-                calculateDaysSinceEpoch("02jan1970").should.equal("1");
+                calculateDaysSinceEpoch("02jan1970").should.equal(1);
             });
-            it('should return "11.016" for "29feb2000"', function () {
-                calculateDaysSinceEpoch("31apr1970").should.equal("11.016");
+            it('should return "11016" for "29feb2000"', function () {
+                calculateDaysSinceEpoch("29feb2000").should.equal(11016);
             });
         });
     });
