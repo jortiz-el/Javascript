3e1f49f49200f554da02e6200e362081b15d3ef9
jortiz-el <ojoao_funkyman@hotmail.com>
2015-10-30 15:27:34 +0100

    revision

--- solicitudrevisionvehiculo/js/solicitud_revision_vehiculos.js
+++ solicitudrevisionvehiculo/js/solicitud_revision_vehiculos.js
@@ -1,4 +1,14 @@
 
+
 function showVehicleRevisionStatus() {
+    var NUMBERPLATE = new RegExp("(^[A-Z]{1,2}\-\\d{4}\-([A-N]|[O-Z]){1,2}$)|" +
+            "(^\\d{4}\-([A-N]|[O-Z]){3}$)", "i");
+
+    var query = document.location.search;
+    if (query !== ''){
+
+        document.write("<br>" + query);
+    }
     
+
 }
