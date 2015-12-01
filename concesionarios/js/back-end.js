adaa17a9f3658e39ca196c11a9c63564701b983a
jortiz-el <ojoao_funkyman@hotmail.com>
2015-12-01 21:10:56 +0100

    con el concesionario

--- concesionarios/js/back-end.js
+++ concesionarios/js/back-end.js
@@ -1,7 +1,7 @@
 /*global
     exports */
 
-var MODULENAME = "taxes",
+var MODULENAME = "backEnd",
     haveExports = typeof exports,
     container;
 
@@ -17,7 +17,37 @@ if (haveExports === "undefined") {
 (function (module) {
     "use strict";
 
-    module.calculateTax = function (tax, price) {
-        return price * (tax / 100);
+    module.CarDealership =  function (red) {
+    	this.red = red;
+    	this.Vehicles = []
     };
+    module.Vehicle =  function (model, numberplate, dateLastrevDate, buy_price, sell_price) {
+		this.model;
+		this.numberplate;
+		this.dateLastrevDate;
+		this.buy_price;
+		this.sell_price;
+    };
+    module.CarDealership.prototype.buy_cars = function buy_cars(model, numberplate, dateLastrevDate, buy_price, sell_price) {
+    	this.Vehicles.push(new Vehicle(model, numberplate, dateLastrevDate, buy_price, sell_price));
+    };
+    module.CarDealership.prototype.show_vehicles = function show_vehicles(id, model, numberplate, dateLastrevDate, sell_price) {
+    	//crear tabla coches comprados
+        var row = $(id).insertRow(0),
+            cell = row.insertCell(0),
+            cell1 = row.insertCell(1),
+            cell2 = row.insertCell(2),
+            cell3 = row.insertCell(3),
+            cell4 = row.insertCell(4);
+        //añadir texto
+        cell1.innerHTML = model;
+        cell2.innerHTML = numberplate;
+        cell3.innerHTML = dateLastrevDate;
+        cell4.innerHTML = sell_price;
+
+    };
+
+
+
+
 }(container));
--- concesionarios/js/front-end.js
+++ concesionarios/js/front-end.js
@@ -17,19 +17,8 @@
             red = form.red.value || form.red.placeholder;
         $("titulo").innerHTML = choose_title;
         $("titulo_red").innerHTML = title + red;
-        //crear tabla coches comprados
-        var row = $("comprados").insertRow(0),
-            cell = row.insertCell(0),
-            cell1 = row.insertCell(1),
-            cell2 = row.insertCell(2),
-            cell3 = row.insertCell(3),
-            cell4 = row.insertCell(4);
-        //añadir texto
-        cell1.innerHTML = "Modelo";
-        cell2.innerHTML = "Matricula";
-        cell3.innerHTML = "F.Revisión";
-        cell4.innerHTML = "Precio";
-
+        backEnd.CarDealership.show_vehicles("comprados", "modelo", "matricula", "fecha", "precio");
+        
     }
 
     module.onload = function () {
