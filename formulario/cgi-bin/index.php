74c72570ffa4100e81364983fe85056eaf6f7e38
jortiz-el <ojoao_funkyman@hotmail.com>
2016-01-20 21:32:36 +0100

    validandop

--- formulario/cgi-bin/index.php
+++ formulario/cgi-bin/index.php
@@ -7,8 +7,8 @@ if(!isset($_COOKIE[$cookie_name])) {
     echo "Cookie named '" . $cookie_name . "' is not set!";
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 } else {
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
+    //echo "Cookie '" . $cookie_name . "' is set!<br>";
+    //echo "Value is: " . $_COOKIE[$cookie_name];
 }
 
 if (isset($_POST['name'])) {
--- formulario/html/js/form.js
+++ formulario/html/js/form.js
@@ -2,43 +2,31 @@
 function $(id) {
 	return document.getElementById(id);
 }
+var NAME = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/,
+	SURNAME = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/,
+	PASS = /(?=^.{6,255}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,  
+	EMAIL = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+	WEBSITE = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
 $("acept_cookie").addEventListener("click",hide,false);
-$("name").addEventListener("keyup",name,false);
-$("surname").addEventListener("keyup",surname,false);
-$("pass").addEventListener("keyup",pass,false);
+$("name").addEventListener("keyup",function(){validInput(NAME,$("name"));},false);
+$("surname").addEventListener("keyup",function(){validInput(SURNAME,$("surname"));},false);
+$("pass").addEventListener("keyup",function(){validInput(PASS,$("pass"));},false);
 $("conf_pass").addEventListener("keyup",conf_pass,false);
 $("country").addEventListener("change",country,false);
+$("email").addEventListener("keyup",function(){validInput(EMAIL,$("email"));},false);
+$("website").addEventListener("keyup",function(){validInput(WEBSITE,$("website"));},false);
 
 
 function hide() {
 	$("cookie_form").style.display = "none";
 }
-function name() {
-	var patt = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/,
-		valid = patt.test($("name").value);
+function validInput(patt,id) {
+	var valid = patt.test(id.value);
 		if (valid) {
-			$("name").setCustomValidity("");
+			id.setCustomValidity("");
 		} else {
-			$("name").setCustomValidity("Invalid field.");
-		}
-}
-function surname() {
-	var patt = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/,
-		valid = patt.test($("surname").value);
-		if (valid) {
-			$("surname").setCustomValidity("");
-		} else {
-			$("surname").setCustomValidity("Invalid field.");
-		}
-}
-function pass() {
-	var patt =/(?=^.{6,255}$)((?=.*\d)(?=.*\s)(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*\s)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*\s)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*/,
-		valid = patt.test($("pass").value);
-		if (valid) {
-			$("pass").setCustomValidity("");
-		} else {
-			$("pass").setCustomValidity("Invalid field.");
+			id.setCustomValidity("Invalid field.");
 		}
 }
 function conf_pass() {
@@ -58,3 +46,4 @@ function country() {
 		$("spain").style.display = "none";
 	}
 }
+
